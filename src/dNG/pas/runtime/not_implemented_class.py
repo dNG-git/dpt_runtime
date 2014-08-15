8de673741ff387b9c1befc732834f573b41a6b9f
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-15 19:43:04 +0200

    Enhancement: Added "NotImplementedClass" used for features not being implemented on a given platform

---
+++ src/dNG/pas/runtime/not_implemented_class.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.py?pas;core
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.py?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(pasCoreVersion)#
+#echo(__FILEPATH__)#
+"""
+
+from .not_implemented_exception import NotImplementedException
+
+class NotImplementedClass(NotImplementedException):
+#
+	"""
+The "NotImplementedClass" is used for features not available or implemented
+on a specific installation.
+
+:author:     direct Netware Group
+:copyright:  direct Netware Group - All rights reserved
+:package:    pas
+:subpackage: core
+:since:      v0.1.01
+:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+             Mozilla Public License, v. 2.0
+	"""
+
+	def __init__(self, *args, **kwargs):
+	#
+		"""
+Constructor __init__(NotImplementedClass)
+
+:since: v0.1.01
+		"""
+
+		raise NotImplementedException()
+	#
+
+	def __getattr__(self, name):
+	#
+		"""
+python.org: Called when an attribute lookup has not found the attribute in
+the usual places (i.e. it is not an instance attribute nor is it found in the
+class tree for self).
+
+:param name: Attribute name
+
+:return: (mixed) Instance attribute
+:since:  v0.1.01
+		"""
+
+		raise NotImplementedException()
+	#
+#
+
+##j## EOF
