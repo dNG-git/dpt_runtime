d5e66e3f433f70f5e70c8d8ad4d4f15449fcef07
NotTheEvilOne <git-NTEO@vplace.de>
2018-08-04 12:12:59 +0200

    Enhancement: Add support for additional "setup.py" commands

--- setup.py
+++ setup.py
@@ -16,13 +16,18 @@ https://www.direct-netware.de/redirect?licenses;mpl2
 setup.py
 """
 
-from os import path
+from os import makedirs, path
 
 from distutils.core import setup
 
-from dNG.distutils.command.build_py import BuildPy
-from dNG.distutils.command.install_data import InstallData
-from dNG.distutils.temporary_directory import TemporaryDirectory
+try:
+    from dNG.distutils.command.build_py import BuildPy
+    from dNG.distutils.command.install_data import InstallData
+    from dNG.distutils.command.sdist import Sdist
+    from dNG.distutils.temporary_directory import TemporaryDirectory
+except ImportError:
+    raise RuntimeError("'dng-builder-suite' prerequisite not matched")
+#
 
 def get_version():
     """
@@ -36,37 +41,38 @@ Returns the version currently in development.
 #
 
 with TemporaryDirectory(dir = ".") as build_directory:
-    parameters = { "install_data_plain_copy_extensions": "json",
-                   "pasCoreVersion": get_version()
-                 }
+    parameters = { "pasCoreVersion": get_version(), "plain_copy_extensions": "json" }
+
+    BuildPy.set_build_target_path(build_directory)
+    BuildPy.set_build_target_parameters(parameters)
 
     InstallData.add_install_data_callback(InstallData.plain_copy, [ "data", "lang" ])
     InstallData.set_build_target_path(build_directory)
     InstallData.set_build_target_parameters(parameters)
 
-    _build_path = path.join(build_directory, "src")
+    Sdist.set_build_target_path(build_directory)
+    Sdist.set_build_target_parameters(parameters)
 
-    setup(name = "pas-core",
-          version = get_version(),
-          description = "Python Application Services",
-          long_description = """"pas_core" provides multiple features used in other modules.""",
-          author = "direct Netware Group et al.",
-          author_email = "web@direct-netware.de",
-          license = "MPL2",
-          url = "https://www.direct-netware.de/redirect?pas;core",
+    makedirs(path.join(build_directory, "src", "dNG"))
 
-          platforms = [ "any" ],
+    _setup = { "name": "pas-core",
+               "version": get_version(),
+               "description": "Python Application Services",
+               "long_description": """"pas_core" provides multiple features used in other modules.""",
+               "author": "direct Netware Group et al.",
+               "author_email": "web@direct-netware.de",
+               "license": "MPL2",
+               "url": "https://www.direct-netware.de/redirect?pas;core",
 
-          setup_requires = "dng-builder-suite",
+               "platforms": [ "any" ],
 
-          package_dir = { "": _build_path },
-          packages = [ "dNG" ],
+               "packages": [ "dNG" ],
 
-          data_files = [ ( "docs", [ "LICENSE", "README" ]) ],
+               "data_files": [ ( "docs", [ "LICENSE", "README" ]) ]
+             }
 
-          # Override build_py to first run builder.py over all PAS modules
-          cmdclass = { "build_py": BuildPy,
-                       "install_data": InstallData
-                     }
-         )
+    # Override build_py to first run builder.py
+    _setup['cmdclass'] = { "build_py": BuildPy, "install_data": InstallData, "sdist": Sdist }
+
+    setup(**_setup)
 #
